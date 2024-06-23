2e0b191647ab6835942290c816fbc679fd9d9858
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-06-23 18:14:55 +0530

    first commit

---
+++ Paralax-scroll/script.js
@@ -0,0 +1,34 @@
+let listBg = document.querySelectorAll('.banner .bg');
+let titleBanner = document.querySelector('.banner h1');
+let listTab = document.querySelectorAll('.tab');
+window.addEventListener('scroll', (event) => {
+    // scrollY in the web Scrollbar position (pixel)
+    let top = this.scrollY;
+    /*
+    index is the order of class bg(0,1,2....8)
+    when Scrolling the web, the class bg scroll down..,
+    the bigger the index, the faster the movement....
+    */
+    listBg.forEach((bg, index) => {
+        if (index != 0 && index != 8) {
+            bg.style.transform = `translateY(${top * index / 2}px)`;
+        }
+        else if (index == 0) {
+            bg.style.transform = `translateY(${top / 3}px)`;
+        }
+    })
+    titleBanner.style.transform = `translateY(${top * 4 / 2}px)`;
+    //parallax srcoll in tap
+    // when tabs positionn is less than 500px
+    //from scrollbar position add active class to animetion
+    // and vice versa
+    listTab.forEach(tab => {
+        if (tab.offsetTop - top < 550) {
+            tab.classList.add('active');
+        }
+        else {
+            tab.classList.remove('active');
+        }
+    })
+
+})
